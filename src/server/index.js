3dcd78cee688920fcc96bbb9cc0b5cfee72ce9ec
kaiching0109 <kaiching0109@gmail.com>
2019-04-25 01:15:05 -0400

    DEPLOY: change port to 5000

--- src/server/index.js
+++ src/server/index.js
@@ -67,6 +67,6 @@ app.post("/api/mail", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log(`Listening on port ${process.env.PORT || 8080}!`)
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Listening on port ${process.env.PORT || 5000}!`)
 );
